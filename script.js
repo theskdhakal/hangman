18a8216218c0f6bdedd8a2d5629ec2112f4e77d8
Shiva Dhakal <theskdhakal@gmail.com>
2023-02-14 13:40:27 +1100

    hangman just started

---
+++ script.js
@@ -0,0 +1,6 @@
+const letterContainer = document.getElementById("letter-contianer");
+const optionsContainer = document.getElementById("options-container");
+const userInputSection = document.getElementById("user-input-section");
+const newGameContainer = document.getElementById("new-game-container");
+const newGameButton = document.getElementById("new-game-button");
+const canvas = document.getElementById("canvas");
